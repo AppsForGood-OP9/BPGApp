45dc03d55897139ca05688d9a4876536bc3c2362
Talia-OP9 <trsmith@wpi.edu>
2021-05-04 13:14:09 -0400

    BPData file

--- app/src/main/java/com/example/bpgapp/bpData.java
+++ app/src/main/java/com/example/bpgapp/bpData.java
@@ -15,8 +15,20 @@ public class bpData implements Serializable {
         private int ID;
 
         //Create text column
-        @ColumnInfo(name = "bpText")
-        private String bpText;
+        @ColumnInfo(name = "systolicText")
+        private String systolicText;
+
+        @ColumnInfo(name = "diastolicText")
+        private String diastolicText;
+
+        @ColumnInfo(name = "date")
+        private String date;
+
+        @ColumnInfo(name = "time")
+        private String time;
+
+        @ColumnInfo(name = "notesText")
+        private String notesText;
 
         //Add more fields later
         //Generate getter and setter
@@ -29,12 +41,50 @@ public class bpData implements Serializable {
             this.ID = ID;
         }
 
-        public String getBpText() {
-            return bpText;
+        public String getSystolicText() {
+            return systolicText;
         }
 
-        public void setBpText(String text) {
-            this.bpText = text;
+        public void setSystolicText(String text) {
+            this.systolicText = text;
         }
 
+        public String getDiastolicText() {
+            return diastolicText;
+        }
+
+        public void setDiastolicText(String text) {
+            this.diastolicText = text;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public void setDate(String text) {
+            this.date = text;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String text) {
+            this.time = text;
+        }
+
+    public String getNotesText() {
+        return notesText;
     }
+
+    public void setNotesText(String text) {
+        this.notesText = text;
+    }
+
+        //Might have to get older tables cleared out
+        //Remove other values
+        //Might have schema problem
+        //Completely delete the app
+
+
+}
