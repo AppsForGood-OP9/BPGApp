e7d05d396f0ac3a8d5152fff96ef48bd1bf8fee6
Talia-OP9 <trsmith@wpi.edu>
2021-05-04 13:46:29 -0400

    BPDao file

--- app/src/main/java/com/example/bpgapp/bpDao.java
+++ app/src/main/java/com/example/bpgapp/bpDao.java
@@ -23,11 +23,33 @@ public interface bpDao {
     @Delete
     void reset(List<bpData> bpData);
 
+    //Update systolic
+    @Query("UPDATE bp_table SET systolicText = :sText WHERE ID = :sID")
+    void updateSystolic(int sID, String sText);
+
+    //Update diastolic
+    @Query("UPDATE bp_table SET diastolicText = :dText WHERE ID = :dID")
+    void updateDiastolic(int dID, String dText);
+
+    //Update notes
+    @Query("UPDATE bp_table SET notesText = :nText WHERE ID = :nID")
+    void updateNotes(int nID, String nText);
+
     //Update query
-    @Query("UPDATE bp_table SET bpText = :sText WHERE ID = :sID")
-    void update(int sID, String sText);
+    @Query("UPDATE bp_table SET systolicText = :sText, diastolicText =:dText WHERE ID = :sID")
+    void updateBoth(int sID, String sText, String dText);
+    //@Query("UPDATE bp_table SET diastolicText =:dText WHERE ID =:dID")
+    //void update(int dID, String dText);
+
+    //Update query with all data
+    @Query("UPDATE bp_table SET systolicText = :sText, diastolicText =:dText, notesText =:nText WHERE ID =:sID")
+    void updateAll(int sID, String sText, String dText, String nText);
 
     //Get all data query
     @Query("SELECT * FROM bp_table")
     List<bpData> getAll();
+    //Use in other classes
+    //Could loop over it because it's in the list
+    //Print it just to make sure
+    //Do select star from
 }
